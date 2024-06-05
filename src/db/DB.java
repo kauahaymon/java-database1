0ff0ad19abd9fac86bdfd27ca296cb17d30f872b
Kauã Haymon <kauahaymon@hotmail.com>
2024-06-05 17:58:08 -0400

    Establishing connection, creating methods and so on

---
+++ src/db/DB.java
@@ -0,0 +1,53 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+    // Static methods to connect and disconnect from db
+
+    private static Connection conn = null;
+
+    // Connect to db
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Properties props= loadProperties();
+                String url = props.getProperty("dburl");
+                conn = DriverManager.getConnection(url, props);
+            }
+            catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    // Disconnect from db
+    public static void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            }catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    // Method to load db.properties
+    private static Properties loadProperties() {
+        try (FileInputStream fileInput = new FileInputStream("db.properties")) {
+            Properties properties = new Properties();
+            properties.load(fileInput);
+            return properties;
+        }
+        catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+}
---
+++ src/db/DbException.java
@@ -0,0 +1,9 @@
+package db;
+
+public class DbException extends RuntimeException {
+
+    // Custom Exception error message
+    public DbException(String msg) {
+        super(msg);
+    }
+}
